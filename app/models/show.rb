4db90b5d849e6d25d4ce14e13a292e1ae3520fd6
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-23 00:35:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,11 +21,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-
+    self.where("rating > ?", 5)
   end
 
   def self.shows_by_alphabetical_order
-
+    self.order(:name)
   end
 
 end
